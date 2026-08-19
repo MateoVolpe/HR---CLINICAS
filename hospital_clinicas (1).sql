2f980d5ff2415c022a8157a45e294c442d4eb4cf
MateoVolpe <matuvolpemerello@gmail.com>
2026-08-19 11:02:46 -0300

    Add files via upload
    subimos base de datos

---
+++ hospital_clinicas (1).sql
@@ -0,0 +1,378 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-08-2026 a las 16:00:40
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `hospital_clinicas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `acompanantes`
+--
+
+CREATE TABLE `acompanantes` (
+  `id_acompanante` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `apellido` varchar(100) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ambulancias`
+--
+
+CREATE TABLE `ambulancias` (
+  `id_ambulancia` int(11) NOT NULL,
+  `matricula` varchar(7) DEFAULT NULL,
+  `modelo` varchar(100) DEFAULT NULL,
+  `estado` enum('Disponible','En traslado','Mantenimiento') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `conductores`
+--
+
+CREATE TABLE `conductores` (
+  `id_conductor` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `apellido` varchar(100) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `documentos`
+--
+
+CREATE TABLE `documentos` (
+  `id_documento` int(11) NOT NULL,
+  `titulo` varchar(200) NOT NULL,
+  `descripcion` text DEFAULT NULL,
+  `archivo` varchar(255) NOT NULL,
+  `codigo_qr` varchar(255) NOT NULL,
+  `fecha_subida` datetime DEFAULT current_timestamp(),
+  `id_funcionario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `elementos`
+--
+
+CREATE TABLE `elementos` (
+  `id_elemento` int(11) NOT NULL,
+  `tipo` enum('Paciente','Material Biológico','Equipamiento Médico','Insumo') DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `encuestas`
+--
+
+CREATE TABLE `encuestas` (
+  `id_encuesta` int(11) NOT NULL,
+  `titulo` varchar(150) DEFAULT NULL,
+  `descripcion` text DEFAULT NULL,
+  `fecha_creacion` date DEFAULT NULL,
+  `id_funcionario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `funcionarios`
+--
+
+CREATE TABLE `funcionarios` (
+  `id_funcionario` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `contrasena` varchar(255) NOT NULL,
+  `cargo` varchar(50) NOT NULL,
+  `estado` enum('Activo','Inactivo') DEFAULT 'Activo'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `funcionarios`
+--
+
+INSERT INTO `funcionarios` (`id_funcionario`, `nombre`, `apellido`, `usuario`, `contrasena`, `cargo`, `estado`) VALUES
+(1, 'a', 'a', 'a', 'a', 'a', 'Activo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `preguntas`
+--
+
+CREATE TABLE `preguntas` (
+  `id_pregunta` int(11) NOT NULL,
+  `id_encuesta` int(11) DEFAULT NULL,
+  `pregunta` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuestas`
+--
+
+CREATE TABLE `respuestas` (
+  `id_respuesta` int(11) NOT NULL,
+  `id_pregunta` int(11) DEFAULT NULL,
+  `respuesta` text DEFAULT NULL,
+  `fecha` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rutas`
+--
+
+CREATE TABLE `rutas` (
+  `id_ruta` int(11) NOT NULL,
+  `origen` varchar(200) DEFAULT NULL,
+  `destino` varchar(200) DEFAULT NULL,
+  `distancia_km` decimal(6,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `traslados`
+--
+
+CREATE TABLE `traslados` (
+  `id_traslado` int(11) NOT NULL,
+  `id_ambulancia` int(11) DEFAULT NULL,
+  `id_conductor` int(11) DEFAULT NULL,
+  `id_acompanante` int(11) DEFAULT NULL,
+  `id_elemento` int(11) DEFAULT NULL,
+  `origen` varchar(200) DEFAULT NULL,
+  `destino` varchar(200) DEFAULT NULL,
+  `hora_salida` datetime DEFAULT NULL,
+  `hora_llegada` datetime DEFAULT NULL,
+  `estado` enum('Pendiente','En camino','Llegó al destino','Regresando','Finalizado') DEFAULT 'Pendiente',
+  `id_funcionario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `acompanantes`
+--
+ALTER TABLE `acompanantes`
+  ADD PRIMARY KEY (`id_acompanante`);
+
+--
+-- Indices de la tabla `ambulancias`
+--
+ALTER TABLE `ambulancias`
+  ADD PRIMARY KEY (`id_ambulancia`),
+  ADD UNIQUE KEY `matricula` (`matricula`);
+
+--
+-- Indices de la tabla `conductores`
+--
+ALTER TABLE `conductores`
+  ADD PRIMARY KEY (`id_conductor`);
+
+--
+-- Indices de la tabla `documentos`
+--
+ALTER TABLE `documentos`
+  ADD PRIMARY KEY (`id_documento`),
+  ADD KEY `id_funcionario` (`id_funcionario`);
+
+--
+-- Indices de la tabla `elementos`
+--
+ALTER TABLE `elementos`
+  ADD PRIMARY KEY (`id_elemento`);
+
+--
+-- Indices de la tabla `encuestas`
+--
+ALTER TABLE `encuestas`
+  ADD PRIMARY KEY (`id_encuesta`),
+  ADD KEY `id_funcionario` (`id_funcionario`);
+
+--
+-- Indices de la tabla `funcionarios`
+--
+ALTER TABLE `funcionarios`
+  ADD PRIMARY KEY (`id_funcionario`),
+  ADD UNIQUE KEY `usuario` (`usuario`);
+
+--
+-- Indices de la tabla `preguntas`
+--
+ALTER TABLE `preguntas`
+  ADD PRIMARY KEY (`id_pregunta`),
+  ADD KEY `id_encuesta` (`id_encuesta`);
+
+--
+-- Indices de la tabla `respuestas`
+--
+ALTER TABLE `respuestas`
+  ADD PRIMARY KEY (`id_respuesta`),
+  ADD KEY `id_pregunta` (`id_pregunta`);
+
+--
+-- Indices de la tabla `rutas`
+--
+ALTER TABLE `rutas`
+  ADD PRIMARY KEY (`id_ruta`);
+
+--
+-- Indices de la tabla `traslados`
+--
+ALTER TABLE `traslados`
+  ADD PRIMARY KEY (`id_traslado`),
+  ADD KEY `id_ambulancia` (`id_ambulancia`),
+  ADD KEY `id_conductor` (`id_conductor`),
+  ADD KEY `id_acompanante` (`id_acompanante`),
+  ADD KEY `id_elemento` (`id_elemento`),
+  ADD KEY `id_funcionario` (`id_funcionario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `acompanantes`
+--
+ALTER TABLE `acompanantes`
+  MODIFY `id_acompanante` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ambulancias`
+--
+ALTER TABLE `ambulancias`
+  MODIFY `id_ambulancia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `conductores`
+--
+ALTER TABLE `conductores`
+  MODIFY `id_conductor` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `documentos`
+--
+ALTER TABLE `documentos`
+  MODIFY `id_documento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `elementos`
+--
+ALTER TABLE `elementos`
+  MODIFY `id_elemento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `encuestas`
+--
+ALTER TABLE `encuestas`
+  MODIFY `id_encuesta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `funcionarios`
+--
+ALTER TABLE `funcionarios`
+  MODIFY `id_funcionario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `preguntas`
+--
+ALTER TABLE `preguntas`
+  MODIFY `id_pregunta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `respuestas`
+--
+ALTER TABLE `respuestas`
+  MODIFY `id_respuesta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `rutas`
+--
+ALTER TABLE `rutas`
+  MODIFY `id_ruta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `traslados`
+--
+ALTER TABLE `traslados`
+  MODIFY `id_traslado` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `documentos`
+--
+ALTER TABLE `documentos`
+  ADD CONSTRAINT `documentos_ibfk_1` FOREIGN KEY (`id_funcionario`) REFERENCES `funcionarios` (`id_funcionario`);
+
+--
+-- Filtros para la tabla `encuestas`
+--
+ALTER TABLE `encuestas`
+  ADD CONSTRAINT `encuestas_ibfk_1` FOREIGN KEY (`id_funcionario`) REFERENCES `funcionarios` (`id_funcionario`);
+
+--
+-- Filtros para la tabla `preguntas`
+--
+ALTER TABLE `preguntas`
+  ADD CONSTRAINT `preguntas_ibfk_1` FOREIGN KEY (`id_encuesta`) REFERENCES `encuestas` (`id_encuesta`);
+
+--
+-- Filtros para la tabla `respuestas`
+--
+ALTER TABLE `respuestas`
+  ADD CONSTRAINT `respuestas_ibfk_1` FOREIGN KEY (`id_pregunta`) REFERENCES `preguntas` (`id_pregunta`);
+
+--
+-- Filtros para la tabla `traslados`
+--
+ALTER TABLE `traslados`
+  ADD CONSTRAINT `traslados_ibfk_1` FOREIGN KEY (`id_ambulancia`) REFERENCES `ambulancias` (`id_ambulancia`),
+  ADD CONSTRAINT `traslados_ibfk_2` FOREIGN KEY (`id_conductor`) REFERENCES `conductores` (`id_conductor`),
+  ADD CONSTRAINT `traslados_ibfk_3` FOREIGN KEY (`id_acompanante`) REFERENCES `acompanantes` (`id_acompanante`),
+  ADD CONSTRAINT `traslados_ibfk_4` FOREIGN KEY (`id_elemento`) REFERENCES `elementos` (`id_elemento`),
+  ADD CONSTRAINT `traslados_ibfk_5` FOREIGN KEY (`id_funcionario`) REFERENCES `funcionarios` (`id_funcionario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

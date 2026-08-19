b49fbaebb9dad8e49aeae9be299a1f64244f5acd
joaquinsamudio76 <joaquinsamudio76@gmail.com>
2026-08-19 11:38:35 -0300

    registro de ambulancia

---
+++ js/registrar_ambulancia.js
@@ -0,0 +1,29 @@
+const matricula = document.getElementById('matricula');
+const modelo = document.getElementById('modelo');
+const estado = document.getElementById('estado');
+const boton = document.getElementById('boton');
+
+boton.addEventListener('click', async (e) => {
+
+    e.preventDefault();
+
+    let doc = new FormData();
+
+    doc.append('matricula', matricula.value);
+    doc.append('modelo', modelo.value);
+    doc.append('estado', estado.value);
+
+    let respuesta = await fetch('../php/registrar_ambulancia.php', {
+        method: 'POST',
+        body: doc
+    });
+
+    let texto = await respuesta.text();
+
+    if (texto.trim() === 'ok') {
+        alert('Ambulancia guardada correctamente');
+    } else {
+        alert('Error al guardar la ambulancia');
+    }
+
+});
---
+++ php/registtrarambulancia.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'conexion.php';
+
+$nombre = $_POST['id_ambulancia'];
+$apellido = $_POST['matricula'];
+$usuario = $_POST['modelo'];
+$pass = $_POST['estado'];
+
+
+
+$sql = "INSERT INTO ambulancia (id_ambulancia, matricula, modelo, estado) 
+        VALUES ('$matricula', '$id_ambulancia', '$modelo', '$estado')";
+
+
+if ($con->query($sql)) {
+    echo "ok";
+} else {
+    echo "error";
+}

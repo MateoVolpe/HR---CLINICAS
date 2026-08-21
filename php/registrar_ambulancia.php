454a1feb1e618e681fc9638837d0501bf0b3c4bc
joaquinsamudio76 <joaquinsamudio76@gmail.com>
2026-08-21 11:06:42 -0300

    cambios en registro ambulancia

--- php/registrar_ambulancia.php
+++ php/registrar_ambulancia.php
@@ -1,15 +1,14 @@
 <?php
 require_once 'conexion.php';
 
-$nombre = $_POST['id_ambulancia'];
 $apellido = $_POST['matricula'];
 $usuario = $_POST['modelo'];
 $pass = $_POST['estado'];
 
 
 
-$sql = "INSERT INTO ambulancias (id_ambulancia, matricula, modelo, estado) 
-        VALUES ('$matricula', '$id_ambulancia', '$modelo', '$estado')";
+$sql = "INSERT INTO ambulancias (matricula, modelo, estado) 
+        VALUES ('$matricula', '$modelo', '$estado')";
 
 
 if ($con->query($sql)) {
